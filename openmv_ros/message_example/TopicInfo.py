448492b716f14817f9e6c8cdcbca18f80314d00d
Alexis Paques <alexis.paques@gmail.com>
2017-06-02 13:02:13 +0200

    Adding TopicInfo handcrafted message

---
+++ openmv_ros/message_example/TopicInfo.py
@@ -0,0 +1,136 @@
+from microros import Struct
+
+class TopicInfo(object):
+  _md5sum = "0ad51f88fc44892f8c10684077646005"
+  _type = "rosserial_msgs/TopicInfo"
+  _has_header = False #flag to mark the presence of a Header object
+  _full_text = """# special topic_ids
+uint16 ID_PUBLISHER=0
+uint16 ID_SUBSCRIBER=1
+uint16 ID_SERVICE_SERVER=2
+uint16 ID_SERVICE_CLIENT=4
+uint16 ID_PARAMETER_REQUEST=6
+uint16 ID_LOG=7
+uint16 ID_TIME=10
+uint16 ID_TX_STOP=11
+
+# The endpoint ID for this topic
+uint16 topic_id
+
+string topic_name
+string message_type
+
+# MD5 checksum for this message type
+string md5sum
+
+# size of the buffer message must fit in
+int32 buffer_size
+"""
+  # Pseudo-constants
+  ID_PUBLISHER = 0
+  ID_SUBSCRIBER = 1
+  ID_SERVICE_SERVER = 2
+  ID_SERVICE_CLIENT = 4
+  ID_PARAMETER_REQUEST = 6
+  ID_LOG = 7
+  ID_TIME = 10
+  ID_TX_STOP = 11
+
+  __slots__ = ['topic_id','topic_name','message_type','md5sum','buffer_size']
+  _slot_types = ['uint16','string','string','string','int32']
+
+  def __init__(self, *args, **kwds):
+    self.topic_id = 0
+    self.topic_name = ''
+    self.message_type = ''
+    self.md5sum = ''
+    self.buffer_size = 0
+
+  def serialize(self, buff):
+    """
+    serialize message into buffer
+    :param buff: buffer, ``StringIO``
+    """
+    try:
+      buff.write(_struct_H.pack(self.topic_id))
+      _x = self.topic_name
+      length = len(_x)
+      if python3 or type(_x) == unicode:
+        _x = _x.encode('utf-8')
+        length = len(_x)
+      if python3:
+        buff.write(struct.pack('<I%sB'%length, length, *_x))
+      else:
+        buff.write(struct.pack('<I%ss'%length, length, _x))
+      _x = self.message_type
+      length = len(_x)
+      if python3 or type(_x) == unicode:
+        _x = _x.encode('utf-8')
+        length = len(_x)
+      if python3:
+        buff.write(struct.pack('<I%sB'%length, length, *_x))
+      else:
+        buff.write(struct.pack('<I%ss'%length, length, _x))
+      _x = self.md5sum
+      length = len(_x)
+      if python3 or type(_x) == unicode:
+        _x = _x.encode('utf-8')
+        length = len(_x)
+      if python3:
+        buff.write(struct.pack('<I%sB'%length, length, *_x))
+      else:
+        buff.write(struct.pack('<I%ss'%length, length, _x))
+      buff.write(_struct_i.pack(self.buffer_size))
+    except:
+      pass
+
+  def deserialize(self, str):
+    """
+    unpack serialized message in str into this message instance
+    :param str: byte array of serialized message, ``str``
+    """
+    try:
+      end = 0
+      start = end
+      end += 2
+      (self.topic_id,) = _struct_H.unpack(str[start:end])
+      start = end
+      end += 4
+      (length,) = _struct_I.unpack(str[start:end])
+      start = end
+      end += length
+      if python3:
+        self.topic_name = str[start:end].decode('utf-8')
+      else:
+        self.topic_name = str[start:end]
+      start = end
+      end += 4
+      (length,) = _struct_I.unpack(str[start:end])
+      start = end
+      end += length
+      if python3:
+        self.message_type = str[start:end].decode('utf-8')
+      else:
+        self.message_type = str[start:end]
+      start = end
+      end += 4
+      (length,) = _struct_I.unpack(str[start:end])
+      start = end
+      end += length
+      if python3:
+        self.md5sum = str[start:end].decode('utf-8')
+      else:
+        self.md5sum = str[start:end]
+      start = end
+      end += 4
+      (self.buffer_size,) = _struct_i.unpack(str[start:end])
+      return self
+    except:
+      pass
+
+  def __str__(self):
+    return "Topic ID {0}, topic_name {1}, message_type {2}, md5sum {3}, buffer_size {4}".format(self.topic_id, self.topic_name, self.message_type, self.md5sum, self.buffer_size)
+
+_struct_I = Struct('<I')
+_struct_i = Struct("<i")
+_struct_H = Struct("<H")
--- openmv_ros/microros/microros.py
+++ openmv_ros/microros/microros.py
@@ -3,14 +3,31 @@ import struct
 class rospy(object):
   _publishers = []
   _subscribers = []
+  _toPublish = []
 
   def __init__(self):
     self.usb = pyb.USB_VCP()
     self.usb.setinterrupt(-1)
 
-
   def spinOnce(self):   
     # Handle serial communication
+    """
+      1st Byte - Sync Flag (Value: 0xff)
+      2nd Byte - Sync Flag / Protocol version
+      3rd Byte - Message Length (N) - Low Byte
+      4th Byte - Message Length (N) - High Byte
+      5th Byte - Checksum over message length
+      6th Byte - Topic ID - Low Byte - 0
+      7th Byte - Topic ID - High Byte - 1
+      N Bytes  - Serialized Message Data - (2 + N-1)
+      Byte N+8 - Checksum over Topic ID and Message Data N+2
+    """
+
+    # Publishers
+    while len(self._toPublish) > 0:
+        msg = self._toPublish.pop()
+
+    # Subscribers
     if self.usb.any():
         header = bytes(4)
         read_bytes = self.usb.readinto(header)
@@ -28,43 +45,70 @@ class rospy(object):
             self.usb.readall()
             return
 
-        dataLength = int(header[2]) + int(header[3])
+        dataLength = int(header[2]) + int(header[3])*256
         
-        if header[5] != checksum(buff):
+        if header[4] != self.checksum(header):
             self.usb.readall()
             return
 
-        data = bytes(dataLength)
-        read_bytes = self.usb.readinto(data)
+        # Topic ID + checksum => dataLength+3
+        message_data = bytes(dataLength+3)
+        read_bytes = self.usb.readinto(message_data)
 
-        if read_bytes < len(data):
+        if read_bytes < len(message_data):
             self.usb.readall()
             return
+
+        topic_id = int(message_data[0]) + int(message_data[1])*256
+
+        if message_data[dataLength-1] != self.checksum(message_data):
+            self.usb.readall()
+            return
+
+        if topic_id == 0:
+          return self.send_topics()
+
+        topic_id = int(message_data[0]) + int(message_data[1])*256
+
+        for sub in self._subscribers:
+            if sub.topic_id == topic_id:
+                result = sub.datatype()
+                result.deserialize(message_data[2:-1])
+                break;
 
   def advertise(self, _publisher):
-    pass
+    self._publishers.append(_publisher)
+    pass # Send advertisement through USB
 
   def subscribe(self, _subscriber):
-    pass
+    self._subscribers.append(_subscriber)
+    pass # Send subscibe through USB
 
   def checksum(self, buff):
-    _result = 255 - (sum(buff)%256)
+    _result = 255 - (sum(buff[:-1])%256)
     return _result
+
+  def send_topics(self):
+    pass
 
 class Publisher(object):
   topic_id = None
 
   def __init__(self, topic_name, datatype)
+    self.datatype = datatype
+    self.topic_name = topic_name
     pass
 
   def publish(self):
-    pass
+    
 
 class Subscriber(object):
   topic_id = None
 
   def __init__(self, topic_name, datatype, callback)
-    pass
+    self.datatype = datatype
+    self.topic_name = topic_name
+    self.callback = callback
 
 
 class Struct:
